febe38f9650e9dda7993b56ece6d5263d143a235
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2023-12-26 18:04:05 +0700

    release: 1.0.0

--- notarizer.sh
+++ notarizer.sh
@@ -2,16 +2,16 @@
 
 # Check if the correct number of arguments were provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <zip_path> <notary_profile>"
+    echo "Usage: $0 <dmg_path> <notary_profile>"
     exit 1
 fi
 
 # Assign the arguments to variables
-ZIP_PATH="$1"
+DMG_PATH="$1"
 NOTARY_PROFILE="$2"
 
-# Notarize the zip
-xcrun notarytool submit "$ZIP_PATH" \
+# Notarize the DMG
+xcrun notarytool submit "$DMG_PATH" \
   --keychain-profile "$NOTARY_PROFILE" \
   --wait
 
